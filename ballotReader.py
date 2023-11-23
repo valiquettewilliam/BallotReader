2d816b0fa501a37738e7cd4cb9da6590e9181def
Will <valiquettewilliam@gmail.com>
2023-11-23 11:20:15 -0500

    printing count

--- ballotReader.py
+++ ballotReader.py
@@ -1,11 +1,26 @@
-
+from collections import defaultdict
 
 def BallotCount(filename):
+    candidateDict = defaultdict(int)
+    votersSet= set()
+
     with open(filename) as txt_file:
         for line in txt_file:
-            print(line)
+            vote = line.split()
+            if len(vote) != 2 :
+                # print error of text formating
+                pass
+            if  vote[0] in votersSet:
+                # print fraud detection
+                pass
+            else:
+                votersSet.add(vote[0])
 
+            candidateDict[vote[1]] +=1
 
+    
+    print(candidateDict)
+    print(votersSet)
 
 if __name__ == "__main__":
     BallotCount("ballotData.txt")
