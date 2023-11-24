deab2319b6076a7036dd4e02cba958f2f68907b1
Will <valiquettewilliam@gmail.com>
2023-11-24 12:05:48 -0500

    added exception handling and comments

--- ballotReader.py
+++ ballotReader.py
@@ -3,26 +3,39 @@ import operator
 import pandas as pd
 
 def BallotCount(filename):
+    """ 
+		The function read from a txt file and return the 3 candidates with the most votes. 
+
+		Parameters: 
+			filename (string): The txt file to read from. 
+		
+		Returns: 
+			list[tuple]: list of lenght 3 that contains tuple as (CandidateID, votes) 
+	"""
     candidateDict = defaultdict(int)
     votersSet= set()
+    lineNumber = 0
 
     with open(filename) as txt_file:
         for line in txt_file:
+            lineNumber+=1
             # ignore empty lines
             if line == "" or line.isspace():
                 continue 
 
             vote = line.split(",")
             if len(vote) != 2 :
-                print("WARNING: line wrongly formatted data. Data red: " + line)
+                print(f"WARNING: line wrongly formatted data at line {lineNumber}. Data red: {line}")
                 continue
             if  vote[0] in votersSet:
                 print(f"Oops: there seems to be a frauder. The voter {vote[0]} attempted to vote more than once." )
                 continue
 
             votersSet.add(vote[0])
-            # maybe check for a casting error here 
-            candidateDict[int(vote[1])] +=1
+            try:
+                candidateDict[int(vote[1])] +=1
+            except ValueError:
+                print(f"WARNING: Found vote data which is not a number at line {lineNumber}. Data red: {line}")
 
     # sort by value in reverse order
     sortedList = sorted(candidateDict.items(), key=operator.itemgetter(1), reverse=True)
@@ -30,7 +43,10 @@ def BallotCount(filename):
     return sortedList[:3]
 
 
-# If we were to read it not like a stream
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                       Pandas DataFrame section
+#               If we were to read it not like a stream
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 class BallotReader:
     COLUMS_NAMES = ["voderId","candidateId"]
 
@@ -78,9 +94,11 @@ def BallotCountPanda(filename):
 if __name__ == "__main__":
     # BallotCountPanda("ballotData.txt")
 
+    help(BallotCount)
+
     winners = BallotCount("ballotData.txt")
     print(f"""The winners are: 
-          First place:{winners[0][0]}  with {winners[0][1]} votes.
-          Second place:{winners[1][0]} with {winners[1][1]} votes.
-          Third place:{winners[2][0]}  with {winners[2][1]} votes.  """)
+          First place:  {winners[0][0]}  with {winners[0][1]} votes.
+          Second place: {winners[1][0]}  with {winners[1][1]} votes.
+          Third place:  {winners[2][0]}  with {winners[2][1]} votes.  """)
 
